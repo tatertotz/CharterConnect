3f8600f901fd8c58783dc1d86bd84d9946c46239
tatertotz <Student@Avalons-Mac-mini.local>
2017-02-16 11:32:37 -0600

    Made a ScreenConfigurations class to maniupulate buttons' dimensions and locations

---
+++ app/src/main/java/com/example/delia_grimes/charterconnect/ScreenConfigurations.java
@@ -0,0 +1,19 @@
+package com.example.delia_grimes.charterconnect;
+
+/**
+ * Created by Student on 2/16/17.
+ */
+
+public class ScreenConfigurations {
+
+    public int buttonX;
+    public int buttonY;
+    public int buttonHeight;
+    public int buttonWidth;
+
+
+    public class ResourceMainPage extends ScreenConfigurations {
+
+    }
+}
+
