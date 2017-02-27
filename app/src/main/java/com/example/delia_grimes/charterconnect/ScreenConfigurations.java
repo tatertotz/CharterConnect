e0503462a4576ed18d64ad6db5f39e4e56514a09
Alia Tang <lia@Evans-iMac.local>
2017-02-27 16:53:53 -0600

    I changed the fiButtons method so that you can specify which button you would like to fix. I also made it so that when you set the buttons, it calls the fixButtons method

--- app/src/main/java/com/example/delia_grimes/charterconnect/ScreenConfigurations.java
+++ app/src/main/java/com/example/delia_grimes/charterconnect/ScreenConfigurations.java
@@ -20,47 +20,54 @@ public class ScreenConfigurations {
     }
 
 
-    //Putting buttons into the array
+    //Putting buttons into the array and setting their width and height
     public void setButton0(Button button0){
         resourceMainButtons[0] = button0;
+        fixButtons(0);  //Calls the fixButtons method which resets the width and height of this button
     }
 
     public void setButton1(Button button1){
         resourceMainButtons[1] = button1;
+        fixButtons(1);
     }
 
     public void setButton2(Button button2){
         resourceMainButtons[2] = button2;
+        fixButtons(2);
     }
 
     public void setButton3(Button button3){
         resourceMainButtons[3] = button3;
+        fixButtons(3);
     }
 
     public void setButton4(Button button4){
         resourceMainButtons[4] = button4;
+        fixButtons(4);
     }
 
     public void setButton5(Button button5){
         resourceMainButtons[5] = button5;
+        fixButtons(5);
     }
 
     public void setButton6(Button button6){
         resourceMainButtons[6] = button6;
+        fixButtons(6);
     }
 
 
-    private void fixButtons(){
+    private void fixButtons(int button){
         int buttonHeight = (int)(screenHeight*0.25);    //Making a variable for the height of the buttons
         int buttonWidth = (int)(screenWidth*0.25); //Making a variable for the width of the buttons
-        resourceMainButtons[2].setWidth(Math.min(buttonHeight, buttonWidth));
-        resourceMainButtons[2].setHeight(Math.min(buttonHeight, buttonWidth));
-        resourceMainButtons[3].setWidth(Math.min(buttonHeight, buttonWidth));
-        resourceMainButtons[3].setHeight(Math.min(buttonHeight, buttonWidth));
-        resourceMainButtons[4].setWidth(Math.min(buttonHeight, buttonWidth));
-        resourceMainButtons[4].setHeight(Math.min(buttonHeight, buttonWidth));
-        resourceMainButtons[5].setWidth(Math.min(buttonHeight, buttonWidth));
-        resourceMainButtons[5].setHeight(Math.min(buttonHeight, buttonWidth));
+        resourceMainButtons[button].setWidth(Math.min(buttonHeight, buttonWidth));
+        resourceMainButtons[button].setHeight(Math.min(buttonHeight, buttonWidth));
+//        resourceMainButtons[3].setWidth(Math.min(buttonHeight, buttonWidth));
+//        resourceMainButtons[3].setHeight(Math.min(buttonHeight, buttonWidth));
+//        resourceMainButtons[4].setWidth(Math.min(buttonHeight, buttonWidth));
+//        resourceMainButtons[4].setHeight(Math.min(buttonHeight, buttonWidth));
+//        resourceMainButtons[5].setWidth(Math.min(buttonHeight, buttonWidth));
+//        resourceMainButtons[5].setHeight(Math.min(buttonHeight, buttonWidth));
     }
 
 
