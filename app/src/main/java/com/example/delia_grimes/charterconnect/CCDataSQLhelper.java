0c6eddb38878539599a2e5a8c95168bb2ddd1f15
Alia Tang <lia@Evans-iMac.local>
2017-06-03 15:53:05 -0500

    I added in some data for a school manually into the schools table of the database.

--- app/src/main/java/com/example/delia_grimes/charterconnect/CCDataSQLhelper.java
+++ app/src/main/java/com/example/delia_grimes/charterconnect/CCDataSQLhelper.java
@@ -1,5 +1,6 @@
 package com.example.delia_grimes.charterconnect;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -50,6 +51,18 @@ public class CCDataSQLhelper extends SQLiteOpenHelper {
         db.execSQL(CharterConnectDataSQL.Events.SQL_CREATE_ENTRIES);
         db.execSQL(CharterConnectDataSQL.Resources.SQL_CREATE_ENTRIES);
         db.execSQL(CharterConnectDataSQL.Schools.SQL_CREATE_ENTRIES);
+
+        //For now, I'm just adding in the data from one school right here. Later, it will probably get added in somewhere else.
+        ContentValues avalonValues = new ContentValues();
+        avalonValues.put("schoolName", "Avalon");
+        avalonValues.put("url", "avalonschool.org");
+        avalonValues.put("address", "700 Glendale Street, St. Paul, MN 55114");
+        avalonValues.put("grades", "6-12");
+        avalonValues.put("missionStatement", "Avalon School prepares students for college " +
+                "and life in a strong, nurturing community that inspires active learning, " +
+                "engaged citizenship, and hope for the future.");
+        avalonValues.put("contactInfo", "phone number: 651.649.5495, email: info@avalonschool.org");
+        db.insert("Schools", null, avalonValues);
     }
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // This database is only a cache for online data, so its upgrade policy is
