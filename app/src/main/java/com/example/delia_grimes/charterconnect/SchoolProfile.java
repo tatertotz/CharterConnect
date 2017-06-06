63762e829e6456669580393b9b5768aa71c8fa3f
tatertotz <Student@Avalons-Mac-mini.local>
2017-06-06 15:43:49 -0500

    I'm working on making so that it shows the selected school. Not quite done yet...

--- app/src/main/java/com/example/delia_grimes/charterconnect/SchoolProfile.java
+++ app/src/main/java/com/example/delia_grimes/charterconnect/SchoolProfile.java
@@ -1,5 +1,8 @@
 package com.example.delia_grimes.charterconnect;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +15,32 @@ public class SchoolProfile extends AppCompatActivity {
         setContentView(R.layout.activity_school_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Bundle extras = getIntent().getExtras();
+        String whichSchool = null;
+        if (extras != null) {
+            whichSchool = extras.getString("whichSchool");
+        }
+
+        SQLiteOpenHelper schoolsDatabaseHelper = new CCDataSQLhelper(this);
+        SQLiteDatabase schoolsDatabase = schoolsDatabaseHelper.getReadableDatabase();
+
+        String[] schoolName = {whichSchool};
+        Cursor schoolsCursor = schoolsDatabase.rawQuery("SELECT * FROM Events WHERE schoolName = ?", schoolName);
+
+        final String[] schoolInfoArray = new String[5];
+
+        if (schoolsCursor.moveToFirst()) {
+
+//            schoolInfoArray[0] = "Host School: " + c.getString(c.getColumnIndex("hostSchool"));
+//            schoolInfoArray[1] = "Location: " + c.getString(c.getColumnIndex("location"));
+//            schoolInfoArray[2] = "Date: " + c.getString(c.getColumnIndex("date"));
+//            schoolInfoArray[3] = "Time: " + c.getString(c.getColumnIndex("time"));
+//            schoolInfoArray[4] = "Cost: " + c.getString(c.getColumnIndex("cost"));
+
+            setTitle(schoolsCursor.getString(schoolsCursor.getColumnIndex("schoolName")));
+
+        }
     }
 
 }
